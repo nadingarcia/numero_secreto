a7e5dbda435ecf3073bfe834943a920d743a7648
nadingarcia <159864236+nadingarcia@users.noreply.github.com>
2024-02-13 13:40:39 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 function mensagemInicial(){
-    exibirTextoNaTela('h1', 'Jogo da Claudia');
+    exibirTextoNaTela('h1', 'Jogo da Adivinhação');
     exibirTextoNaTela('p', 'Escolha o nivel de dificuldade para começar o jogo');
 }
 
